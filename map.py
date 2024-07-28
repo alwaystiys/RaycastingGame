c04a349f4695479fb7de793e0175f241faad9100
alwaystiys <365825701@qq.com>
2024-07-29 03:30:17 +0800

    add fps control

--- map.py
+++ map.py
@@ -5,14 +5,15 @@ _ = False
 mini_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, 1, 1, 1, 1, 1, _, _, 1, 1, _, 1, 1, _, 1],
-    [1, _, _, _, _, _, 1, _, _, 1, 1, _, 1, 1, _, 1],
-    [1, _, 1, 1, 1, 1, 1, _, _, 1, _, _, _, _, 1, 1],
-    [1, _, 1, _, _, _, 1, _, _, 1, 1, 1, 1, 1, _, 1],
+    [1, _, 2, _, _, _, 1, _, _, _, _, _, 1, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, 5, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, 4, _, _, _, 5, _, _, 1, _, _, 1, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
+
 
 class Map:
     def __init__(self, game):
@@ -21,12 +22,12 @@ class Map:
         self.world_map = {}
         self.get_map()
 
-    def get_map(self): # creates a dictionary of the map for easy access
+    def get_map(self):  # creates a dictionary of the map for easy access
         for y, row in enumerate(self.mini_map):
             for x, value in enumerate(row):
                 if value:
                     self.world_map[(x, y)] = value
-    
-    def draw(self): # draws the map on the screen
-        [pg.draw.rect(self.game.screen, 'red', (pos[0] * 100, pos[1] * 100, 100, 100), 2) 
+
+    def draw(self):  # draws the map on the screen
+        [pg.draw.rect(self.game.screen, 'red', (pos[0] * 100, pos[1] * 100, 100, 100), 2)
          for pos in self.world_map]
--- player.py
+++ player.py
@@ -8,12 +8,11 @@ class Player:
         self.x, self.y = PLAYER_POS
         self.angle = PLAYER_ANGLE
 
-
     def movement(self):
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
         dx, dy = 0, 0
-        speed = PLAYER_SPEED * self.game.delta_time 
+        speed = PLAYER_SPEED * self.game.delta_time
         speed_sin = speed * sin_a
         speed_cos = speed * cos_a
 
@@ -45,28 +44,38 @@ class Player:
 
     def check_wall(self, x, y):
         return (x, y) not in self.game.map.world_map
-    
+
     def check_wall_collision(self, dx, dy):
-        if self.check_wall(int(self.x + dx), int(self.y)):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+        if self.check_wall(int(self.x + dx * scale), int(self.y)):
             self.x += dx
-        if self.check_wall(int(self.x), int(self.y + dy)):
+        if self.check_wall(int(self.x), int(self.y + dy * scale)):
             self.y += dy
 
-
-    def draw(self): 
+    def draw(self):
         # pg.draw.line(self.game.screen, "yellow", (self.x * 100, self.y * 100),
         #              (self.x * 100 + WIDTH * math.cos(self.angle),
         #               self.y * 100 + WIDTH * math.sin(self.angle)), 2)
-        pg.draw.circle(self.game.screen, "blue", (self.x * 100, self.y * 100), 15)
-
+        pg.draw.circle(self.game.screen, "blue",
+                       (self.x * 100, self.y * 100), 15)
         
+    def mouse_control(self):
+        mx, my = pg.mouse.get_pos()
+        if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+        self.ref = pg.mouse.get_rel()[0]
+        self.ref = max(-MOUSE_MAX_REL, min(MOUSE_MAX_REL, self.ref))
+        self.angle += self.ref * MOUSE_SENSITIVITY * self.game.delta_time
+        # self.angle %= math.tau
+
     def update(self):
         self.movement()
-
+        self.mouse_control()
+        
     @property
     def pos(self):
         return self.x, self.y
-    
+
     @property
     def map_pos(self):
         return int(self.x), int(self.y)
